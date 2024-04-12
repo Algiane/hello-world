a765b30a36839900f038dce8fa54451eeac1acb3
Algiane Froehly <algiane@gmail.com>
2024-04-12 12:01:06 +0200

    Add preprocessor branch.

--- hello-world.c
+++ hello-world.c
@@ -4,9 +4,12 @@
 int main(int argc, char* argv[]) {
   int a=2, b=3;
 
+#ifdef BRANCH_1
    printf("Hello, World!\n");
-
+#else
+   printf("Bye, World!\n");
    assert(a<b);
+#endif
 
    return 0;
 }
