d95d149c0f919fd78ef12c97329f5ede4393ee1f
Algiane Froehly <algiane@gmail.com>
2024-04-12 14:29:02 +0200

    Another test.

--- hello-world.c
+++ hello-world.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include<assert.h>
+#include <assert.h>
+#include <stdlib.h>
 
 #ifdef BRANCH_1
 #define VAL 1
@@ -8,13 +9,25 @@
 #endif
 
 int main(int argc, char* argv[]) {
-  int a=2, b=3;
+  int a,b;
+
 
   if ( VAL ) {
     printf("Hello, World!\n");
   }
   else {
    printf("Bye, World!\n");
+   assert(a<b);
+  }
+
+  a = atoi(argv[1]);
+  b = atoi(argv[2]);
+
+  if ( a<b ) {
+    printf("2: Hello, World!\n");
+  }
+  else {
+   printf("2: Bye, World!\n");
    assert(a<b);
   }
 
