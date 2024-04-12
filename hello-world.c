1b4014846797c00d56d97f0b46cb109fac2424f0
Algiane Froehly <algiane@gmail.com>
2024-04-12 12:13:43 +0200

    Test coverage with different branching model.

--- hello-world.c
+++ hello-world.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include<assert.h>
 
+#ifdef BRANCH_1
+#define VAL 1
+#else
+#define VAL 0
+#endif
+
 int main(int argc, char* argv[]) {
   int a=2, b=3;
 
-#ifdef BRANCH_1
-   printf("Hello, World!\n");
-#else
+  if ( VAL ) {
+    printf("Hello, World!\n");
+  }
+  else {
    printf("Bye, World!\n");
    assert(a<b);
-#endif
+  }
 
    return 0;
 }
