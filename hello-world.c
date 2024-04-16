06a9cd93f4cecb9a9a42f0e6b021dabbea1351b2
Algiane Froehly <algiane@gmail.com>
2024-04-16 14:51:14 +0200

    Attempt to add partial coverage.

--- hello-world.c
+++ hello-world.c
@@ -9,17 +9,21 @@
 #endif
 
 int main(int argc, char* argv[]) {
-  int a,b;
+  int a,b,c;
 
   a = atoi(argv[1]);
   b = atoi(argv[2]);
 
+  /* Test coverage eval with preprocessor macros */
   if ( VAL ) {
     printf("Hello, World!\n");
+    assert(a!=0);
+    assert(a!=0 && b!=0);
   }
   else {
-   printf("Bye, World!\n");
-   assert(a!=0 && b!=0);
+    printf("Bye, World!\n");
+    assert(a!=0);
+    assert(a!=0 && b!=0);
   }
 
 
@@ -27,9 +31,34 @@ int main(int argc, char* argv[]) {
     printf("2: Hello, World!\n");
   }
   else {
-   printf("2: Bye, World!\n");
-   assert(a!=0 && b!=0);
+    printf("2: Bye, World!\n");
+    assert(a);
+    assert(a!=0 && b!=0);
   }
 
-   return 0;
+  c = 10;
+
+  /* Partial coverage: branch coverage is false, line coverage too */
+  for (int i=0; i<10; ++i ) {
+    if ( i>c ) {
+      continue;
+    }
+  }
+
+  /* Partial coverage: branch coverage is false: i>c never executed */
+  for (int i=0; i<10; ++i ) {
+    if ( i<c ) {
+      continue;
+    }
+  }
+
+
+  /* Full coverage: both possible branches of the if are executed */
+  for (int i=0; i<20; ++i ) {
+    if ( i<c ) {
+      continue;
+    }
+  }
+
+  return 0;
 }
